6c8a002f575e6e5d4146903aa16ef91ddc4bc163
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-05 19:51:07 +0000

    Completed  javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 2;
+var height = 10;
+var area = width * height;
+console.log('The value of area now is:', area);
+console.log('The type of area is:', typeof area);
+
+var bill = 50;
+var payment = 130;
+var change = payment - bill;
+console.log('Change will be: ', change);
+console.log('Type of change is: ', typeof change);
+
+var quizzes = 83;
+var midterm = 60;
+var final = 76;
+var grade = (quizzes + midterm + final) / 3;
+console.log('The average grade is: ', grade);
+console.log('Type of grade is ', typeof grade);
+
+var firstName = 'Yuliia ';
+var lastName = 'Pchelintseva';
+var fullName = firstName + lastName;
+console.log('Full name is: ', fullName);
+console.log('The type of fullName is: ', typeof fullName);
+
+var pH = 2.5;
+var isAcidic = pH < 7;
+console.log('pH level is below 7: ', isAcidic);
+console.log('Type of isAcidic is: ', typeof isAcidic);
+
+var headCount = 107;
+var isSparta = headCount === 300;
+console.log('Sparta had 300 soldiers in the battle of Thermopylae: ', isSparta);
+console.log('Type of headCount is: ', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('The value of moto now is:', motto);
+console.log('Type of motto is: ', typeof motto);
