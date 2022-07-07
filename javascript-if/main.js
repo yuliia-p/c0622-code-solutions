0e73ad4753a75caa65d6337607661629e59eb8d4
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-07 22:53:32 +0000

    compl javascript-if

---
+++ javascript-if/main.js
@@ -0,0 +1,64 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+function isUnderFive(number) {
+  return number < 5;
+}
+
+function isEven(number) {
+  return (number % 2 === 0);
+}
+
+function startsWithJ(string) {
+  var firstLetter = string[0];
+  return firstLetter === 'J';
+}
+
+function isOldEnoughToDrink(person) {
+  return person.age >= 21;
+}
+
+function isOldEnoughToDrive(person) {
+  return person.age >= 16;
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return !(person.age >= 16) && (person.age >= 21);
+}
+
+var msg = ' ';
+
+function categorizeAcidity(pH) {
+  if (pH === 7) {
+    msg = 'neutral';
+  }
+  if (pH <= 7) {
+    msg = 'acid';
+  }
+  if ((pH <= 14) && (pH >= 7)) {
+    msg = 'base';
+  } else {
+    msg = 'invalid pH level';
+  }
+  return msg;
+}
+
+var catchPhrase = ' ';
+function introduceWarnerBro(name) {
+  if (name === 'yakko') {
+    catchPhrase = "We're the warner brothers!";
+  } else if (name === 'wakko') {
+    catchPhrase = "We're the warner brothers!";
+  } else if (name === 'dot') {
+    catchPhrase = "I'm cute~";
+  } else {
+    catchPhrase = 'Goodnight everybody!';
+  }
+  return catchPhrase;
+}
