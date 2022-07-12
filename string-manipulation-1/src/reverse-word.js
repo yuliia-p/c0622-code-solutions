436b74542f98591208ee892ca5ea58af0c9b64e5
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-12 02:02:38 +0000

    compl string-manipulation-1

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,5 +1,7 @@
 /* exported reverseWord */
-
+// Assign an empty string to variable for a future word
+// Using for loop get all indexes of a character in reverse order
+// Sum and assign all reversed indexes to get a new word
 function reverseWord(word) {
   var newWord = '';
   for (var i = word.length - 1; i >= 0; i--) {
