a8273b1fc046857c22844936dc32a78eeb25f779
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-13 17:36:11 +0000

    working on string-manipulation-2

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,4 +1,7 @@
 /* exported truncate */
+// use substring method on string
+// with start from 0 to given number length
+// return it from a function
 function truncate(length, string) {
-  return string.slice(string.length) + '...';
+  return string.substring(0, length) + '...';
 }
