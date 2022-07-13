fd10fed0435c941a6872b89518e556ad94079a96
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-13 00:39:53 +0000

    compl dom-manipulation

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -2,29 +2,9 @@ var clickCount = 0;
 var buttonEl = document.querySelector('.hot-button');
 var count = document.querySelector('.click-count');
 
-// define a function that
-// increments the counter variable
-// return the increment value form the function
-function buttonClickIncr() {
+function buttonClick() {
   clickCount++;
-
-}
-
-buttonEl.addEventListener('click', buttonClickIncr);
-
-// define a function
-// that updates textContent of the '.click-count' element
-// each time the user clicks
-
-function textCountUpdate() {
   count.textContent = 'Clicks: ' + clickCount;
-  return count.textContent;
-}
-
-buttonEl.addEventListener('click', textCountUpdate);
-
-function classNameUpdate() {
-  clickCount++;
   var a = null;
   if (clickCount < 4) {
     a = 'cold';
@@ -44,4 +24,4 @@ function classNameUpdate() {
   buttonEl.className = 'hot-button ' + a;
 }
 
-buttonEl.addEventListener('click', classNameUpdate);
+buttonEl.addEventListener('click', buttonClick);
