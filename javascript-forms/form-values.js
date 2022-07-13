67f53f2687140e035bdf5800fb98b5bfb993504d
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-13 21:29:43 +0000

    javascript-forms

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,27 +1,25 @@
 
 var elContactForm = document.querySelector('#contact-form');
 
-var userName = elContactForm.elements.name.value;
-// document.querySelector('#user-name').elements;
+// var userName = elContactForm.elements.name.value;
+// // document.querySelector('#user-name').elements;
 
-var userEmail = elContactForm.elements.email.value;
-// document.querySelector('#user-email').elements;
+// var userEmail = elContactForm.elements.email.value;
+// // document.querySelector('#user-email').elements;
 
-var userMessage = elContactForm.elements.message.value;
-// document.querySelector('#user-message').elements;
+// var userMessage = elContactForm.elements.message.value;
+// // document.querySelector('#user-message').elements;
 
 function getValues(event) {
   event.preventDefault();
   var newObj = {};
-  newObj.name = userName;
-  newObj.email = userEmail;
-  newObj.userMessage = userMessage;
+  newObj.name = elContactForm.elements.name.value;
+  newObj.email = elContactForm.elements.email.value;
+  newObj.userMessage = elContactForm.elements.message.value;
 
   // return newObj;
   console.log(newObj);
-
+  elContactForm.reset();
 }
 
 elContactForm.addEventListener('submit', getValues);
-
-elContactForm.reset();
