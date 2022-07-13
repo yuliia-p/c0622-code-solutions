9cec99f013979c43dde39dd42b5ce1a005fbde6a
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-13 00:33:00 +0000

    working on javascript-light-bulb

---
+++ javascript-light-bulb/main.js
@@ -0,0 +1,41 @@
+
+// var clickCount = 0;
+// var switchLight = document.querySelector('.light-bulb-on');
+
+// var background = document.querySelector('.light-on');
+
+// function turnOnOffLight() {
+//   clickCount++;
+//   var currentMode = null;
+
+// }
+
+// if (currentMode) {
+//   switchLight.className = 'light-bulb-on';
+//   background.className = 'light-on display-flex';
+// } else {
+//   switchLight.className = 'light-bulb-off';
+//   background.className = 'light-off display-flex';
+// }
+
+// if ((switchLight.className === 'light-bulb-on light-bulb-off') &&
+//   (background.className === 'light-on light-off display-flex')) {
+//   switchLight.className = 'light-bulb-on';
+//   background.className = 'light-on display-flex';
+// } else {
+//   switchLight.className = 'light-bulb-off';
+//   background.className = 'light-off display-flex';
+// }
+
+// console.log(switchLight.className);
+
+// switchLight.addEventListener('click', turnOnOffLight);
+
+// var el = document.querySelectorAll('.light');
+// if (el.className('.light-on .light-bulb-on')) {
+//   el.className = '.light-off .light-bulb-off';
+// } else {
+//   el.className = '.light-on .light-bulb-on';
+// }
+// && document.querySelector('light-off')) {
+//     document.querySelector('light-off');
