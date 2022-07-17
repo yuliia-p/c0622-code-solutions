6466be9139262019310f9d51991126969998cc2c
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-17 21:44:01 +0000

    string-manipulation-2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,28 +1,29 @@
 /* exported swapChars */
-//
-//
-//
+// SUBSTRING = start (including index), end (excluding index)
+// if firstIndex === secondIndex => return string
+// to use substring method start index should be smaller than end index
+// if not swap them
+// using substring method cut the string
+// into parts - before fisrt given index (from 0 to firstIndex)
+// then from firstIndex + 1 (to avoid character we swaping) to secondIndex
+// and last part - from secondIndex to the end
+// concatenate all part
+// return new string
 
 function swapChars(firstIndex, secondIndex, string) {
   if (firstIndex === secondIndex) {
     return string;
   }
-
-  if (secondIndex < firstIndex) {
-    var temp = secondIndex;
-    secondIndex = firstIndex;
-    firstIndex = temp;
+  var first = firstIndex;
+  var second = secondIndex;
+  if (firstIndex > secondIndex) {
+    first = secondIndex;
+    second = firstIndex;
   }
+  var firstPart = string.substring(0, firstIndex); // comple
+  var secondPart = string.substring(firstIndex + 1, secondIndex); // -
+  var lastPart = string.substring(secondIndex + 1); // d
 
-  if (firstIndex >= string.length) {
-    return string;
-
-  }
-  return string.substring(0, firstIndex) +
-    string[secondIndex] +
-
-    string.substring(firstIndex + 1, secondIndex) +
-
-    string[firstIndex] +
-    string.substring(secondIndex + 1);
+  var newString = firstPart + string[second] + secondPart + string[first] + lastPart;
+  return newString;
 }
