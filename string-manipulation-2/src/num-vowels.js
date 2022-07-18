dcab710549bff7f463c46d6064e39c68f7c1bd12
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-18 16:50:47 +0000

    working on string-manipulation-2

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -3,12 +3,35 @@
 // retun of method is array of vowels
 // .length preperty give us a number of vowels
 
+// function numVowels(string) {
+//   if (!string.match(/[aeiou]/gi)) {
+//     return 0;
+//   } else {
+//     var vowels = string.match(/[aeiou]/gi);
+//     var count = vowels.length;
+//   }
+//   return count;
+// }
 function numVowels(string) {
-  if (!string.match(/[aeiou]/gi)) {
-    return 0;
-  } else {
-    var vowels = string.match(/[aeiou]/gi);
-    var count = vowels.length;
+  var count = 0;
+
+  for (var i = 0; i < string.length; i++) {
+    var char = string[i];
+    if ((char === 'a') || (char === 'A')) {
+      count++;
+    } else if ((char === 'e') || (char === 'E')) {
+      count++;
+    } else if ((char === 'i') || (char === 'I')) {
+      count++;
+    } else if ((char === 'o') || (char === 'O')) {
+      count++;
+    } else if ((char === 'u') || (char === 'U')) {
+      count++;
+    } else if ((char === 'y') || (char === 'Y')) {
+      count++;
+    } else {
+      return count;
+    }
   }
   return count;
 }
