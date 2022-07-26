252d2493a97de66e7c60e7bb236f8eec98d13358
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-07-26 18:18:34 +0000

    javascript-constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var anObject = new Object();
+console.log('value of anObject: ', anObject);
+console.log('typeof anObject:', typeof anObject);
+
+var anArray = new Array();
+console.log('value of anArray: ', anArray);
+console.log('typeof anArray:', typeof anArray);
+
+var aString = new String();
+console.log('value of aString: ', aString);
+console.log('typeof aString:', typeof aString);
+
+var newNumber = new Number();
+console.log('value of newNumber: ', newNumber);
+console.log('typeof newNumber:', typeof newNumber);
+
+var boolean = new Boolean();
+console.log('value of boolean: ', boolean);
+console.log('typeof boolean:', typeof boolean);
+
+var newFun = new Function();
+console.log('value of newFun: ', newFun);
+console.log('typeof newFun:', typeof newFun);
