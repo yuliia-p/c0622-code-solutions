17144935551b896b9666103e09f7866951978961
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-08 20:38:17 +0000

    array-manipulation-3

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,19 +1,28 @@
 // /* exported equal */
-// function equal(first, second) {
-//   if (first.length !== second.length) {
-//     return false;
-//   } else {
-//     for (var i = 0; i < first.length; i++) {
-//       // console.log('first[i]', first[i]);
-//       // console.log('second[i]', second[i]);
-//       // {
-//       // }
-//     }
-//   }
+//
+function equal(first, second) {
+  return first.toString() === second.toString();
+
+  // for (var i = 0; i < second.length; i++) {
+  //   if (first.includes(second[i])) {
+
+  //   }
+  // }
+  // debugger;
+  // if (first.length !== second.length) {
+  //   return false;
+  // } else {
+  //   for (var i = 0; i < second.length; i++) {
+  //     if (first.includes(second[i])) {
+  //       return true;
+  //     }
+  //   }
+  // }
+  // return true;
+}
 //   return (first[i] !== second[i]);
 // }
 // //   }
-// //   // return first.toString() === second.toString();
 // //   for (var i = 0; i < first.length; i++) {
 // //   //   var f = first[i];
 // //   //   for (var x = 0; x < second.length; x++) {
@@ -29,4 +38,4 @@
 
 // // }
 
-// equal([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]);
+equal([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]);
