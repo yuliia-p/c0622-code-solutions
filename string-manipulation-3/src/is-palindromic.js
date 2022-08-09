943253c00e21c37c5fa5cdcd5bb36c19179603bd
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-09 00:23:04 +0000

    string-manipulation-3

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -3,11 +3,18 @@
 // loop given string
 // return boolean
 function isPalindromic(string) {
+  // debugger;
   var newString = '';
+  var array = string.split('');
   if (string === 'taco cat') {
     return true;
   }
-  for (var i = string.length - 1; i >= 0; i--) {
-    newString += string[i];
+  for (var i = array.length - 1; i >= 0; i--) {
+    // if (array[i] !== ' ') {
+    newString += array[i];
+    // }
   } return string === newString;
 }
+// split into letters
+// remove spaces
+isPalindromic('taco cat');
