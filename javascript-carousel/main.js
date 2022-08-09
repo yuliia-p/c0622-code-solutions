eda76a7d0289f2f883e86ee72f59206d35ca5be1
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-09 18:18:04 +0000

    fixed clearInterval

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -4,18 +4,16 @@ var iElements = document.querySelectorAll('.fa-circle'); // all dots NodeList
 var imgEls = document.querySelectorAll('.img'); // img node list
 var lefttArrow = document.querySelector('.fa-chevron-left');
 var currentIndex = 0;
+var intervalId = null;
 
 lefttArrow.addEventListener('click', prevImgRight);
 rightArrow.addEventListener('click', nextImage);
 dotContEl.addEventListener('click', clickOnDot);
-setInterval(nextImage, 3000);
-var intervalId = setInterval(nextImage, 3000);
 
 // previous
 function prevImgRight(event) {
   // when currentIndex becomes 0, jump back to finish
   // clear interval
-  // set up a new one
   clearInterval(intervalId);
   if (currentIndex === 0) {
     imgEls[currentIndex].className = 'img display-none';
@@ -31,12 +29,13 @@ function prevImgRight(event) {
     iElements[currentIndex - 1].className = 'fa-solid fa-circle current';
     currentIndex--;
   }
+  // set up a new one
+  intervalId = setInterval(nextImage, 3000);
 }
 
 function clickOnDot(event) {
   if (event.target.tagName === 'I') {
     // clear interval
-  // set up a new one
     clearInterval(intervalId);
     var buttonId = event.target.getAttribute('data-icon'); // 1-5; id of i el
     var newIndex = buttonId - 1;
@@ -46,11 +45,14 @@ function clickOnDot(event) {
     iElements[newIndex].className = 'fa-solid fa-circle current';
     currentIndex = newIndex;
   }
+  // set up a new one
+  intervalId = setInterval(nextImage, 3000);
 }
 
 // 0 1 2 3 4 -> index
 // 1 2 3 4 5 -> length
 function nextImage(event) {
+  clearInterval(intervalId);
   if (currentIndex === imgEls.length - 1) {
     // jump back to 0
     imgEls[currentIndex].className = 'img display-none';
@@ -66,5 +68,7 @@ function nextImage(event) {
     iElements[currentIndex + 1].className = 'fa-solid fa-circle current';
     currentIndex++;
   }
-  clearInterval(intervalId);
+  intervalId = setInterval(nextImage, 3000);
 }
+
+intervalId = setInterval(nextImage, 3000);
