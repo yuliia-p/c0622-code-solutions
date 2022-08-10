e6ab0241c3d266e3f0daf093b226f32eb1bd2d16
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-10 17:04:32 +0000

    string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,40 +1,31 @@
 // /* exported titleCase */
-// // split str into words
+// // split str into words array
 // // cap first char of each
 // // title = 'javascript: the definitive guide'
 // // words[0] = 'javascript'
 
 function titleCase(title) {
   // debugger;
+  var exceptions = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
   var newArr = [];
-  var words = title.split(' ');
-  for (var i = 0; i < words.length; i++) {
-    if (words[i].length <= 3) {
-      var a = words[i].toLowerCase();
-      newArr.push(a);
-    } else if (words[i].length >= 4) {
-      var capW = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
-      newArr.push(capW);
+  var words = title.split(' '); // ==> ['javascript:', 'the', 'definitive', 'guide']
+  var firstWord = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase(); // first word always starts with capital
+  newArr.push(firstWord);
+  for (var i = 1; i < words.length; i++) {
+    if (exceptions.includes(words[i])) {
+      newArr.push(words[i]);
+    } else {
+      var restWords = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+      newArr.push(restWords);
     }
   }
-  // for (var y = 0; y < newArr.length; y++) {
-  //   if (words[y].toLowerCase === 'javascript') {
-  //     // words[y] = 'JavaScript';
-  //     newArr.splice(y, 1, 'JavaScript');
-  //   }
-  // }
   var str = newArr.join(' ');
   var js = 'Javascript' || 'Javascript:';
   var newStr = str.replace(js, 'JavaScript');
   return newStr;
-  //     // var firstW = words[0].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
-  //     // if (words[i].length >= 4) {
-  //     // var capW = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
-  //     // newArr.push(capW);
-  //     // // }
-  //     // if (words[i] === )
-  //   }
-  //   // newArr.push(firstW);
-
 }
+titleCase('speaking Javascript: an in-depth guide for programmers');
+
+titleCase('hop on pop');
+titleCase('node.js in action');
 titleCase('professional JavaScript for web developers');
