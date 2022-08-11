25b3cfbcb06684a779693ea55e2fd45188ef6415
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-11 17:26:03 +0000

    string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,8 +1,12 @@
 // /* exported titleCase */
-// // split str into words array
-// // cap first char of each
-// // title = 'javascript: the definitive guide'
-// // words[0] = 'javascript'
+// split str into words array
+// cap first char of first word
+//
+//
+// use join method
+// return new str
+// title = 'javascript: the definitive guide'
+// words[0] = 'javascript'
 
 function titleCase(title) {
   // debugger;
@@ -20,9 +24,17 @@ function titleCase(title) {
       splitWordArray.splice(0, 1, splitWordArray[0].toUpperCase());
       splitWordArray.splice(capitalCount, 1, splitWordArray[capitalCount].toUpperCase());
       newArr.push(splitWordArray.join(''));
-    } else if (words[i].includes(':')) {
-      var wordAftercolon = words[i + 1].toUpperCase();
-      newArr.push(wordAftercolon);
+      // skips this step
+    } else if (words[i].includes(':')) { // use split ? words[i - 1] ??
+      // var splitWordArray2 = words[i].split('');
+      var wordWithColon = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+      newArr.push(wordWithColon);
+      // do i need to check if it is already in array
+      var wordAfterColon = words[i + 1];
+      if (exceptions.includes(wordAfterColon)) {
+        newArr.push(wordAfterColon.charAt(0).toUpperCase() + wordAfterColon.slice(1).toLowerCase()); // words[i].charAt(0).toUpperCase())
+      }
+      // var wordAfterColon = words[i + 1].charAt(0).toUpperCase() + words[i + 1].slice(1).toLowerCase(); // words[i].charAt(0).toUpperCase()
     } else {
       var restWords = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
       newArr.push(restWords);
@@ -33,6 +45,8 @@ function titleCase(title) {
   var newStr = str.replace(js, 'JavaScript');
   return newStr;
 }
+titleCase('speaking Javascript: an in-depth guide for programmers');
+
 titleCase('speaking: the in-depth guide for programmers');
 titleCase('hop on pop');
 titleCase('node.js in action');
