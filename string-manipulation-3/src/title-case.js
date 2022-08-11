e1633a6c82e52208a20e8c9383c1edd5789498e2
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-11 23:15:05 +0000

    string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -13,10 +13,16 @@ function titleCase(title) {
   var exceptions = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
   var newArr = [];
   var words = title.split(' '); // ==> ['javascript:', 'the', 'definitive', 'guide']
-  var firstWord = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase(); // first word always starts with capital
-  newArr.push(firstWord);
+  // var firstWord = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase(); // first word always starts with capital
+  // newArr.push(firstWord);
   for (var i = 1; i < words.length; i++) {
-    if (exceptions.includes(words[i])) {
+    if (words[i].includes(':')) {
+      var wordWithColon = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+      newArr.push(wordWithColon);
+      var wordAfterColon = words[i + 1];
+      newArr.push(wordAfterColon.charAt(0).toUpperCase() + wordAfterColon.slice(1).toLowerCase());
+      i++;
+    } else if (exceptions.includes(words[i])) {
       newArr.push(words[i]);
     } else if (words[i].includes('-')) { // in-depth
       var capitalCount = words[i].indexOf('-') + 1;
@@ -24,25 +30,16 @@ function titleCase(title) {
       splitWordArray.splice(0, 1, splitWordArray[0].toUpperCase());
       splitWordArray.splice(capitalCount, 1, splitWordArray[capitalCount].toUpperCase());
       newArr.push(splitWordArray.join(''));
-      // skips this step
-    } else if (words[i].includes(':')) { // use split ? words[i - 1] ??
-      // var splitWordArray2 = words[i].split('');
-      var wordWithColon = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
-      newArr.push(wordWithColon);
-      // do i need to check if it is already in array
-      var wordAfterColon = words[i + 1];
-      if (exceptions.includes(wordAfterColon)) {
-        newArr.push(wordAfterColon.charAt(0).toUpperCase() + wordAfterColon.slice(1).toLowerCase()); // words[i].charAt(0).toUpperCase())
-      }
-      // var wordAfterColon = words[i + 1].charAt(0).toUpperCase() + words[i + 1].slice(1).toLowerCase(); // words[i].charAt(0).toUpperCase()
     } else {
       var restWords = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
       newArr.push(restWords);
     }
   }
   var str = newArr.join(' ');
-  var js = 'Javascript' || 'Javascript:'; // ?
+  var js = 'Javascript';
+  var apiWord = 'Api';
   var newStr = str.replace(js, 'JavaScript');
+  newStr = str.replace(apiWord, 'API');
   return newStr;
 }
 titleCase('speaking Javascript: an in-depth guide for programmers');
