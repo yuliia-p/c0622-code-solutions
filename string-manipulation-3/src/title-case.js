d6429ff68ee8bc0ffdbb07555631b1b029def343
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-13 20:49:46 +0000

    string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,23 +1,21 @@
 // /* exported titleCase */
 // split str into words array
 // cap first char of first word
-//
-//
 // use join method
 // return new str
-// title = 'javascript: the definitive guide'
-// words[0] = 'javascript'
+// var capExceptions = ['and', 'or', 'nor', 'But', 'A', 'An', 'The', 'As', 'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
 
 function titleCase(title) {
-  // debugger;
+  var newTitle = title.toLowerCase();
   var exceptions = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
   var newArr = [];
-  var words = title.split(' '); // ==> ['javascript:', 'the', 'definitive', 'guide']
-  // var firstWord = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase(); // first word always starts with capital
-  // newArr.push(firstWord);
-  for (var i = 1; i < words.length; i++) {
+  var words = newTitle.split(' ');
+  for (var i = 0; i < words.length; i++) {
     if (words[i].includes(':')) {
       var wordWithColon = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+      if (wordWithColon === 'Javascript:') {
+        wordWithColon = 'JavaScript:';
+      }
       newArr.push(wordWithColon);
       var wordAfterColon = words[i + 1];
       newArr.push(wordAfterColon.charAt(0).toUpperCase() + wordAfterColon.slice(1).toLowerCase());
@@ -30,20 +28,31 @@ function titleCase(title) {
       splitWordArray.splice(0, 1, splitWordArray[0].toUpperCase());
       splitWordArray.splice(capitalCount, 1, splitWordArray[capitalCount].toUpperCase());
       newArr.push(splitWordArray.join(''));
+    } else if (words[i].toLowerCase === 'javascript') {
+      newArr.push(words[i] = 'JavaScript');
+    } else if (words[i].toLowerCase() === 'api') {
+      words[i] = 'API';
+      newArr.push(words[i]);
     } else {
       var restWords = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
       newArr.push(restWords);
     }
   }
+  var firstWord = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
+  if (firstWord === 'Javascript') {
+    firstWord = 'JavaScript';
+  } else if (firstWord === 'Javascript:') {
+    firstWord = 'JavaScript:';
+  }
+  newArr.splice(0, 1, firstWord);
   var str = newArr.join(' ');
   var js = 'Javascript';
-  var apiWord = 'Api';
   var newStr = str.replace(js, 'JavaScript');
-  newStr = str.replace(apiWord, 'API');
   return newStr;
 }
+titleCase('web audio api');
+titleCase('javascript: the definitive guide');
 titleCase('speaking Javascript: an in-depth guide for programmers');
-
 titleCase('speaking: the in-depth guide for programmers');
 titleCase('hop on pop');
 titleCase('node.js in action');
