aaafddf2919c0f73b49ed28c864b89926030c4af
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-17 18:48:47 +0000

    node-require

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -3,20 +3,22 @@ const subtract = require('./subtract');
 const multiply = require('./multiply');
 const divide = require('./divide');
 
+const x = Number(process.argv[2]);
+const y = Number(process.argv[4]);
 let result = 0;
 if (process.argv[3] === 'plus') {
-  result = add.add(Number(process.argv[2]), Number(process.argv[4]));
+  result = add.add(x, y);
 }
 
 if (process.argv[3] === 'minus') {
-  result = subtract.subtr(Number(process.argv[2]), Number(process.argv[4]));
+  result = subtract.subtr(x, y);
 }
 
 if (process.argv[3] === 'time') {
-  result = multiply.multip(Number(process.argv[2]), Number(process.argv[4]));
+  result = multiply.multip(x, y);
 }
 
 if (process.argv[3] === 'over') {
-  result = divide.division(Number(process.argv[2]), Number(process.argv[4]));
+  result = divide.division(x, y);
 }
 console.log('result: ', result);
