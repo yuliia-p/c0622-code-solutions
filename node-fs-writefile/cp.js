6b66e9364dc54dd85b29ab8c897aafd61f34ce00
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-18 01:30:36 +0000

    3rd part

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -1,19 +1,12 @@
-// const fs = require('fs');
+const fs = require('fs');
+// console.log(' process.argv', process.argv);
 
-// // fs.readFile(process.argv[2], 'utf8', (err, data) => {
-// //   if (err) throw err;
-// //   console.log(data);
-// // });
-// // console.log('process.argv', process.argv);
-
-// const data = fs.readFile(process.argv[2], 'utf8', (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
-
-// // process.argv[2] + '\n';
-// // copy
-// fs.writeFile(process.argv[3], data, err => {
-//   if (err) throw err;
-//   console.log('Copied');
-// });
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) throw err;
+  // console.log('data:', data);
+  // console.log('process.argv[2]', process.argv[2]);
+  // console.log('typeof process.argv[2]', typeof process.argv[2]);
+  fs.writeFile(process.argv[3], data, err => {
+    if (err) throw err;
+  });
+});
--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -4,5 +4,4 @@ const data = process.argv[2] + '\n';
 
 fs.writeFile('note.txt', data, err => {
   if (err) throw err;
-  // console.log('User\'s input saved to file!');
 });
