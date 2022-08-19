cf3354a8224d33b8b5d520e96d07249c847c753c
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-19 20:24:33 +0000

    completed express-get-json

---
+++ express-get-json/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  function gradesArray(grades) {
+    const newGradesArr = [];
+    for (const key in grades) {
+      newGradesArr.push(key, grades[key]);
+    }
+    return newGradesArr;
+  }
+  res.json(gradesArray(grades));
+});
+
+app.listen(3000, () => {
+  // console.log('Express server listening on port 3000');
+});
