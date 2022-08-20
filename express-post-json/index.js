ffb2819a4526dda9a3859676a5ec2666b6c78520
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-20 01:13:40 +0000

    express-post-json/

---
+++ express-post-json/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+let nextId = 1;
+const grades = {};
+/*
+grades = {
+    "course": "JavaScript",
+    "id": 4,
+    "name": "Yuliia",
+    "score": 2000000
+}
+*/
+app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  const newGradesArr = [];
+  for (const key in grades) {
+    newGradesArr.push(grades[key]);
+  }
+  // newGradesArr.push(grades);
+  res.json(newGradesArr);
+});
+
+app.post('/api/grades', (req, res) => {
+  const gradesObj = req.body; // given obj ???
+  gradesObj.id = nextId;
+  grades[gradesObj.id] = gradesObj;
+  nextId++;
+  res.send(gradesObj);
+  res.status(201);
+});
+
+app.listen(3000, () => {
+  // console.log('Express server listening on port 3000');
+});
