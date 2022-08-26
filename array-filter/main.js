b33c145498b93962892835df59a65bf5e31bac48
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-26 21:09:05 +0000

    compl without challenge

---
+++ array-filter/main.js
@@ -0,0 +1,33 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+
+const evenNumbers = numbers.filter(num => {
+  return num % 2 === 0;
+});
+console.log('evenNumbers: ', evenNumbers);
+
+const overFive = numbers.filter(num => {
+  return num > 5;
+});
+console.log('overFive: ', overFive);
+
+const startWithE = names.filter(names => {
+  return names[0] === 'E';
+});
+console.log('startWithE: ', startWithE);
+
+const haveD = names.filter(names => {
+  return names.includes('D') || names.includes('d');
+});
+console.log('haveD: ', haveD);
