0ce357a918bd124b580407affa1b4aa5a7be1d4d
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-30 22:41:00 +0000

    react-props-and-expressions

--- react-props-and-expressions/src/index.jsx
+++ react-props-and-expressions/src/index.jsx
@@ -2,25 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function CustomButton(props) {
+  const style = { color: props.color, backgroundColor: props.backgroundColor, width: props.width, height: props.height, marginTop: props.marginTop };
   return (
   <div>
       <button
-      style={{
-        color: 'white',
-        backgroundColor: 'darkorchid',
-        width: '100px',
-        height: '40px',
-        marginTop: '30px'
-      }}> {props.text}</button >
+        style={style} >
+      {props.text}</button>
   </div>
   );
 }
 const element = (
   <div>
-    <CustomButton text="Not" color="red" />
-    <CustomButton text="so" />
-    <CustomButton text="Pretty" />
-    <CustomButton text="Buttons" />
+    <CustomButton text="Not" color="white" backgroundColor='darkorchid' width='100px' height='40px' marginTop='30px'/>
+    <CustomButton text="so" color="white" backgroundColor='darkorchid' width='100px' height='40px' marginTop='30px'/>
+    <CustomButton text="Pretty" color="white" backgroundColor='darkorchid' width='100px' height='40px' marginTop='30px'/>
+    <CustomButton text="Buttons" color="white" backgroundColor='darkorchid' width='100px' height='40px' marginTop='30px'/>
   </div>
 );
 const root = ReactDOM.createRoot(document.getElementById('root'));
