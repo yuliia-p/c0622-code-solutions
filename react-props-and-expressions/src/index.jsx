8f22bd3545a2b16f607b353cfd769f79cd915db1
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-30 22:16:11 +0000

    react-props-and-expressions

--- react-props-and-expressions/src/index.jsx
+++ react-props-and-expressions/src/index.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function CustomButton(props) {
-  return <button>{props.text}</button>;
+  return (
+  <div>
+      <button
+      style={{
+        color: 'white',
+        backgroundColor: 'darkorchid',
+        width: '100px',
+        height: '40px',
+        marginTop: '30px'
+      }}> {props.text}</button >
+  </div>
+  );
 }
 const element = (
   <div>
-    <CustomButton text="I" />
-    <CustomButton text="know" />
-    <CustomButton text="React!" />
+    <CustomButton text="Not" color="red" />
+    <CustomButton text="so" />
+    <CustomButton text="Pretty" />
+    <CustomButton text="Buttons" />
   </div>
 );
 const root = ReactDOM.createRoot(document.getElementById('root'));
