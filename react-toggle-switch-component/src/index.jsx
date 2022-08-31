45d5d453ae40e9ec6b3432e5a49f14a9b885bb2a
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-08-31 22:28:07 +0000

    completed react-toggle-switch-component

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,6 @@
+import ToogleSwitch from './toggle-switch';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<ToogleSwitch />);
