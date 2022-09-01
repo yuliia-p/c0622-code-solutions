09693ebb12c4205e85e71ef1a0fb7e53a90a25b7
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-09-01 23:32:07 +0000

    react-validated-input-component

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+function Pokemon(props) {
+  const list = props.pokedex.map(onePokemon =>
+    <li key={onePokemon.number}>
+      {onePokemon.name}
+    </li>);
+  return (
+      <ul>{list}</ul>
+  );
+}
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<Pokemon pokedex={pokedex}/>);
---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+export default class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+  }
+
+  render() {
+    let errText;
+    let iconClassName;
+    const usersInput = this.state.password;
+    if (usersInput.length === 0) {
+      iconClassName = 'fa-xmark';
+      errText = 'A passowrd is required';
+    } else if (usersInput.length < 8) {
+      iconClassName = 'fa-xmark';
+      errText = 'Your password is too short';
+    } else if (!usersInput.includes('!' || '@' || '#' || '$' || '%' || '&' || '*' || '(' || ')')) {
+      iconClassName = 'fa-xmark';
+      errText = 'Please include a special character';
+      // a special character
+    } else {
+      iconClassName = 'fa-check';
+      errText = '';
+    }
+    return (
+      <div>
+      <div className='container'>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="password">Password:</label>
+              <br></br>
+              <input onChange={this.handleChange}
+              id="password" type="text"/>
+          </form>
+        </div>
+        <div>
+          <div className='mark'>
+            <i className={`fa-sharp fa-solid ${iconClassName}`}></i>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>{errText}</p>
+      </div>
+      </div>
+    );
+  }
+}
