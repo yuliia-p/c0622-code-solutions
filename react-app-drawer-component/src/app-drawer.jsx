a0b1332372661f3101682f8e904fbed4e2527484
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-09-01 05:04:57 +0000

    fixed css

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -21,14 +21,14 @@ export default class AppDrawer extends React.Component {
       <div className='container'>
         <div className='icon-div'>
           <i onClick={this.handleClick}
-            className="hover padding-left menu-button fa-solid fa-bars"></i>
+            className="hover margin-left menu-button fa-solid fa-bars"></i>
         </div>
         <div onClick={this.handleClick}
         className={`menu ${menuClassName}`}>
           <div onClick={this.handleClick}
           className="menu-content hover">
-            <h3 className='padding-left'>Menu</h3>
-            <ul className='padding-left'>
+            <h3>Menu</h3>
+            <ul>
               <li onClick={this.handleClick}
               className='hover'>About</li>
               <li onClick={this.handleClick}
