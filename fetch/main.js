9cca8391c3f1e02d25ebb5344adde2a46cd9c930
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-09-02 19:43:59 +0000

    fetch

--- fetch/main.js
+++ fetch/main.js
@@ -8,4 +8,12 @@ fetch('https://pokeapi.co/api/v2/berry/23/')
   .then(data =>
     console.log(data));
 
-//
+fetch('https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=oN0z6slOegD5zjvuGpGHt1uNGJS1uEGE')
+  .then(response => response.json())
+  .then(data =>
+    console.log(data));
+
+fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=oN0z6slOegD5zjvuGpGHt1uNGJS1uEGE')
+  .then(response => response.json())
+  .then(data =>
+    console.log(data));
