efec170678e258bbe34e0c4c1350187b7f1413c1
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-09-02 21:51:45 +0000

    started react-carousel-component

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,28 @@
+// import React from 'react';
+
+// export default class Carousel extends React.Component {
+// render() {
+//   return (
+
+//   )
+// }
+// }
+
+/**
+ * <div className="container">
+      <div className="flex">
+        <i className="fa-solid fa-chevron-left"></i>
+        <div className="image-holder">
+          <img src="../images/001.png" alt="pic">
+        </div>
+        <i class="fa-solid fa-chevron-right"></i>
+      </div>
+      <div class="dot-holder">
+        <i data-icon="1" className="fa-solid fa-circle current"></i>
+        <i data-icon="2" className="fa-solid fa-circle"></i>
+        <i data-icon="3" className="fa-solid fa-circle"></i>
+        <i data-icon="4" className="fa-solid fa-circle"></i>
+        <i data-icon="5" className="fa-solid fa-circle"></i>
+      </div>
+    </div>
+ */
---
+++ react-carousel-component/src/index.jsx
@@ -0,0 +1,6 @@
+import Carousel from './carousel';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<Carousel />);
