bcbd9fd5a0497a6c5801fee4da045afd7b8418c2
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-09-28 17:25:08 +0000

    working on

--- data-structures-linked-lists/without-head.js
+++ data-structures-linked-lists/without-head.js
@@ -1,8 +1,8 @@
 /* exported withoutHead */
 
 function withoutHead(list) {
-  while (list.next.data != null) {
-    list.next.data = list.data;
-  }
-  return list;
+//   while (list.next.data != null) {
+//     list.next.data = list.data;
+//   }
+//   return list;
 }
