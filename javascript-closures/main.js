8041699fe0058680e0c2924c1c9c90b242ac0357
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-09-29 01:14:52 +0000

    javascript-closures

--- javascript-closures/main.js
+++ javascript-closures/main.js
@@ -6,5 +6,9 @@ function graduate(credential) {
 
 const medicalSchool = graduate('M.D.');
 const lawSchool = graduate('Esq.');
+
+console.dir(medicalSchool);
+console.dir(lawSchool);
+
 console.log('medicalSchool:', medicalSchool('Yuliia Kozakova'));
 console.log('lawSchool:', lawSchool('Yuliia Kozakova'));
